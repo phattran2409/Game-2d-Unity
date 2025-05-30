bb298bc732ae7d39ea7b092796628c42beb9e7d6
phattran2409 <phatt649@gmail.com>
2025-05-30 18:36:27 +0700

    Update (customize camare for game block  Y and Z) and modify resolution Full HD)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,31 @@ public class CameraController : MonoBehaviour
     public Transform target; // The target to follow 
     // Start is called once before the first execution of Update after the MonoBehaviour is created 
     private Vector3 velocity = Vector3.zero;
-    private void Update()
+
+    private float initialY;
+    private float initialZ;
+
+    private void Start()
     {
-        Vector3 targetPosition = target.position + offset; // Calculate the target position based on the target's position and the offset
-        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);   
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.3f); // Smoothly move the camera to the target position
+        initialY = transform.position.y;
+        initialZ = transform.position.z;
+
+    }
+
+    //private void Update()
+    //{
+    //    Vector3 targetPosition = target.position + offset; // Calculate the target position based on the target's position and the offset
+    //    //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);   
+    //    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.3f); // Smoothly move the camera to the target position
+    //}
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 targetPosition = new Vector3(target.position.x, initialY, initialZ);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, 0.125f);
+
+        transform.position = smoothedPosition;
     }
 }
