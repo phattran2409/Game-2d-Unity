4a16011a48f701bcb009d4cf0608cdd515ec6a46
phattran2409 <phatt649@gmail.com>
2025-07-13 17:49:39 +0700

    Add : Add message at scene 2

---
+++ Assets/Scripts/Checkpoint/CheckPointMessage.cs
@@ -0,0 +1,77 @@
+﻿using Assets.Scripts.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Checkpoint
+{
+    public class CheckPointMessage : MonoBehaviour
+    {   
+        public TypeWritterEffect typewriter;
+        [TextArea] public List<String> messages = new List<String>();
+        int currentMessage = 0; 
+        private bool isDialogActive = false;    
+        private bool triggered = false;
+        [Header("Input action ")]
+        public InputActionAsset inputActions; // <-- kéo GameInput vào
+        private InputAction nextDialogueAction;
+
+        private void OnEnable()
+        {
+            nextDialogueAction = inputActions.FindAction("UI/NextDialog");
+
+            if (nextDialogueAction != null)
+            {
+                nextDialogueAction.performed += OnNextDialogue;
+                nextDialogueAction.Enable();
+            }
+
+        }
+
+        private void OnDisable()
+        {
+            if (nextDialogueAction != null)
+            {
+                nextDialogueAction.performed -= OnNextDialogue;
+                nextDialogueAction.Disable();
+            }
+        }   
+
+        private void OnNextDialogue(InputAction.CallbackContext ctx)
+        {
+            if (isDialogActive)
+            {
+                ShowNextMessage();
+            }
+        }   
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!triggered && other.CompareTag("Player"))
+            {
+                triggered = true;
+                currentMessage = 0;
+                ShowNextMessage();
+            }
+        }
+ 
+
+        private void ShowNextMessage()
+        {
+            if (currentMessage < messages.Count)
+            {
+                isDialogActive = true;
+                typewriter.ShowText(messages[currentMessage]);
+                currentMessage++;
+            }
+            else
+            {
+                isDialogActive = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Message/TypeWritterEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Message
+{
+    public class TypeWritterEffect : MonoBehaviour
+    {
+        public TextMeshProUGUI textMesh;
+        public GameObject panel;
+        public float delay = 0.03f;
+
+        private Coroutine currentRoutine;
+
+        public void ShowText(string message)
+        {
+            if (currentRoutine != null)
+                StopCoroutine(currentRoutine);
+
+            currentRoutine = StartCoroutine(TypeRoutine(message));
+        }
+
+        private IEnumerator TypeRoutine(string message)
+        {
+            panel.SetActive(true);
+            textMesh.text = "";
+
+            foreach (char c in message)
+            {
+                textMesh.text += c;
+                yield return new WaitForSeconds(delay);
+            }
+
+            yield return new WaitForSeconds(2f);
+            panel.SetActive(false);
+        }
+    }
+}
+    
